fc809257a4d75dfc6a330a62f413871cbbe81310
Mailis Kokla <mailis@fatarrow.net>
2021-04-21 10:50:05 +0300

    Add jest

---
+++ tests/unit/TaskCard.spec.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+import { mount } from '@vue/test-utils'
+import TaskCard from '@/components/TaskCard.vue'
+
+describe('TaskCard.vue', () => {
+  it('should set correct classes for task', async () => {
+    // set custom task object to propsData
+    // check if wrapper has correct color class (for example purple task has to have border-purple-400 class)
+  })
+
+  it('should display date in correct format', async () => {
+    // set custom task object to propsData
+    // check if date element has correct text
+  })
+})
