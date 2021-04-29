bfb10537d52d20b7aebacedcb3a5015ed4f435f6
Mailis Kokla <mailis@fatarrow.net>
2021-04-29 11:46:35 +0300

    Add downloadFile

--- src/components/DownloadFile.vue
+++ src/components/DownloadFile.vue
@@ -1,42 +1,6 @@
 <template>
-  <div class="bg-white shadow rounded px-3 pt-3 pb-5 border border-white">
-    <div class="row">
-      <div class="col">
-        <label for="title" class="block text-sm font-medium text-gray-700">Download file(s)</label>
-
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-      </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col">
-        <label for="type" class="inline-block text-sm font-medium text-gray-700">File type</label>
-        <select v-model="form.fileType" name="type" class="mt-1 block w-full border-gray p-1">
-          <option value="PDF">PDF</option>
-          <option value="XLSX">XLSX</option>
-        </select>
-      </div>
-    </div>
-    <div class="row mt-3">
-      <div class="col">
-        <label for="priority" class="inline-block text-sm font-medium text-gray-700">Statuses to include in the file</label>
-        <div>
-          <label class="inline-flex items-center mt-2">
-            <input type="checkbox" class="form-checkbox h-5 w-5 text-red-600" v-model="form.todo"><span class="ml-2 text-gray-700">Todo</span>
-          </label> <br/>
-          <label class="inline-flex items-center mt-1">
-            <input type="checkbox" class="form-checkbox h-5 w-5 text-red-600" v-model="form.done"><span class="ml-2 text-gray-700">Done</span>
-          </label>
-        </div>
-      </div>
-    </div>
-    <div class="row mt-5">
-      <div class="col text-right">
-        <button class="bg-green-400 px-4 py-2 rounded" @click="getFile">Download</button>
-      </div>
-    </div>
+  <div>
+   <!-- TODO: Create visual logic for downloading file -->
   </div>
 </template>
 <script>
